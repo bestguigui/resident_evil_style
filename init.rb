cb7ea7e652cc17b4a9095504f9d0ceeb08888703
bestguigui <guillaume.quillet@yahoo.fr>
2019-01-15 14:36:32 +0100

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,25 @@
+require 'gosu'
+
+class Window < Gosu::Window
+  def initialize
+    super(640, 480, false)
+    self.caption = 'BoI clone'
+  end
+
+  def needs_cursor?; true; end
+
+  def button_down(id)
+    super
+    close! if id == Gosu::KB_ESCAPE
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+end
+
+Window.new.show
