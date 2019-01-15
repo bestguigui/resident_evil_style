b6c4a55b3ff1e0ec45410452314ef94707943dda
bestguigui <guillaume.quillet@yahoo.fr>
2019-01-15 23:47:33 +0100

    introduced json format and refactoring

--- init.rb
+++ init.rb
@@ -1,3 +1,10 @@
+=begin
+
+TODO : finaliser les directions du personnage
+et sortir le personnage vers une classe Personnage !	
+	
+=end
+
 require 'gosu'
 require 'opengl'
 require 'glu'
@@ -8,19 +15,14 @@ GLU.load_lib
 include OpenGL, GLU
 
 require_relative 'mouse.rb'
+require_relative 'camera.rb'
 
 class Window < Gosu::Window
   def initialize
-    super(640, 480, false)
+    super(800, 600, false)
     self.caption = 'Resident Evil Style'
-
-    mouse_init
-  end
-
-  def mouse_init
-    self.mouse_x = self.width / 2
-    self.mouse_y = self.height / 2
-    @mouse       = Mouse.new(self)
+  	@character = Gosu::Image.new('characters/test.png', retro: true)
+    @camera = Camera.new(self, 'camera.json')
   end
 
   def needs_cursor?; true; end
@@ -31,12 +33,57 @@ class Window < Gosu::Window
   end
 
   def update
-    @mouse.update
-    self.caption = @mouse
+  	@plane_size = 32
+  	@plane_coords ||= [0, 0, 16]
+
+  	vel = 1.0
+  	if (Gosu::button_down?(Gosu::KB_LEFT) or Gosu::button_down?(Gosu::KB_RIGHT))
+      if (Gosu::button_down?(Gosu::KB_UP) or Gosu::button_down?(Gosu::KB_DOWN))
+        vel *= 0.7
+      end
+	end
+
+	# en fonction de la rotation Z, on doit définir les incréments de tile à effectuer... pas simple !
+	
+  end
+
+  def opengl_init
+	glEnable(GL_DEPTH_TEST)
+	glEnable(GL_TEXTURE_2D)
+  end
+
+  def draw_character
+  	angles = @camera.get_angles
+  	glPushMatrix
+  	  glTranslatef(*@plane_coords)    
+  	  glRotatef(angles[2], 0, 0, 1)
+	  glRotatef(angles[0] - 90, 1, 0, 0)
+	  glScalef(@plane_size, @plane_size, @plane_size)
+
+	  tex = @character.gl_tex_info
+	  glBindTexture(GL_TEXTURE_2D, tex.tex_name)
+	  l, r, t, b = tex.left, tex.right, tex.top, tex.bottom
+
+	  glEnable(GL_ALPHA_TEST)
+	  glAlphaFunc(GL_GREATER, 0)
+	    glBegin(GL_QUADS)
+	      glTexCoord2d(l, b); glVertex3f(-0.5, 0.0, 0.0)
+	      glTexCoord2d(l, t); glVertex3f(-0.5, 0.0, 1.0)
+	      glTexCoord2d(r, t); glVertex3f(0.5, 0.0, 1.0)
+	      glTexCoord2d(r, b); glVertex3f(0.5, 0.0, 0.0)
+	    glEnd
+	  glDisable(GL_ALPHA_TEST)
+	glPopMatrix
   end
 
   def draw
+  	gl(10) do
+      opengl_init
+      @camera.look
+      draw_character
+  	end
 
+  	@camera.draw
   end
 end
 
--- mouse.rb
+++ mouse.rb
@@ -1,4 +1,5 @@
 class Mouse
+  attr_reader :angle
   def initialize(window)
     @window = window
     @x      = @window.mouse_x
@@ -8,7 +9,7 @@ class Mouse
 
   def update
     if Gosu::button_down?(Gosu::MS_LEFT) and (@x != @window.mouse_x or @y != @window.mouse_y)
-      @angle = Gosu::angle(@x, @y, @window.mouse_x, @window.mouse_y)
+      @angle = Gosu::angle(@window.mouse_x, @window.mouse_y, @x, @y) + 90
       @x     = @window.mouse_x
       @y     = @window.mouse_y
     end
